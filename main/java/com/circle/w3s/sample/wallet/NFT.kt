0d4186860c96e2ae92ce98b369b78d4ac15cdad9
SinghShubhamkumarkrishnadev <118460329+SinghShubhamkumarkrishnadev@users.noreply.github.com>
2024-05-02 15:45:19 +0530

    Delete main directory

--- main/java/com/circle/w3s/sample/wallet/NFT.kt
+++
@@ -1,3 +0,0 @@
-package com.circle.w3s.sample.wallet
-
-data class NFT(val name: String, val description: String, val imageResId: Int)
--- main/java/com/circle/w3s/sample/wallet/Transactions.kt
+++
@@ -1,7 +0,0 @@
-package com.circle.w3s.sample.wallet
-
-data class Transactions(
-    val recipient: String,
-    val amount: String,
-    val status: String
-)
